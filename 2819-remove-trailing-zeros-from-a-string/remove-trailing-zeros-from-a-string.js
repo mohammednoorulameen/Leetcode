27cfde07a8852cd9c88a4069dcb3fb717e378ce1
MOHAMMED NOORUL AMEEN <96298671+mohammednoorulameen@users.noreply.github.com>
2025-12-16 19:17:31 +0530

    Time: 0 ms (100.00%) | Memory: 55.7 MB (88.65%) - LeetSync

--- 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.js
+++ 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.js
@@ -3,5 +3,12 @@
  * @return {string}
  */
 var removeTrailingZeros = function(num) {
-     return num.replace(/0+$/, '');
+    //  return num.replace(/0+$/, '');
+     let i = num.length - 1;
+
+    while (i >= 0 && num[i] === '0') {
+        i--;
+    }
+
+    return num.slice(0, i + 1);
 };
